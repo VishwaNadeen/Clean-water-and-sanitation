ad3c67dbe3a00e676c5aca4041760c9f56fb8056
TharushikaLakmini <tharushikalakmini1@gmail.com>
2026-03-21 22:55:07 +0530

    Remove commented-

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -13,13 +13,3 @@ cloudinary.config({
 
 export default cloudinary;
 
-// src/config/cloudinary.js
-//import { v2 as cloudinary } from "cloudinary";
-
-//cloudinary.config({
-  //cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  //api_key: process.env.CLOUDINARY_API_KEY,
-  //api_secret: process.env.CLOUDINARY_API_SECRET,
-//});
-
-//export default cloudinary;
